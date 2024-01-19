c1b8676eac3950d659b10dde332dcd64c4a5b1fc
KIMSUNGHO <KIMSUNGHO@DESKTOP-5K6P4QT>
2024-01-19 16:40:27 +0900

    first up

---
+++ ticket24/src/main/java/kr/spring/Ticket24Application.java
@@ -0,0 +1,13 @@
+package kr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ticket24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ticket24Application.class, args);
+	}
+
+}
