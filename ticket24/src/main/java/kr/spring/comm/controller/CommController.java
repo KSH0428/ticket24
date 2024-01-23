4a44e16949eeda098ac5db5740089765b5f0a033
KIMSUNGHO <KIMSUNGHO@DESKTOP-5K6P4QT>
2024-01-23 22:37:46 +0900

    커뮤니티 작업중 조금

---
+++ ticket24/src/main/java/kr/spring/comm/controller/CommController.java
@@ -0,0 +1,25 @@
+package kr.spring.comm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import kr.spring.comm.service.CommService;
+import kr.spring.comm.vo.CommVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class CommController {
+	@Autowired
+	private CommService commService;
+	
+	/*=======================================
+	 * 게시판 글 등록
+	 *======================================*/
+	//자바빈(VO)초기화
+	@ModelAttribute
+	public CommVO initCommand() {
+		return new CommVO();
+	}
+}
